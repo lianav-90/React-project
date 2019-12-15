08fc3a284b047eea5f3659c76c5da590e1239083
Liana <liana-as90@rambler.ru>
2019-12-15 22:13:33 +0400

    Updates

---
+++ precache-manifest.6e3ba77b45e69eea4223887105b72ec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b8928caf6d49b5a4273c8f1ba32518",
+    "url": "/react-homework-4/index.html"
+  },
+  {
+    "revision": "830e90ec173d9a08f2d2",
+    "url": "/react-homework-4/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "ef8e1bd9d960f4c1f8af",
+    "url": "/react-homework-4/static/css/main.cba5ed20.chunk.css"
+  },
+  {
+    "revision": "830e90ec173d9a08f2d2",
+    "url": "/react-homework-4/static/js/2.1cb089a7.chunk.js"
+  },
+  {
+    "revision": "ef8e1bd9d960f4c1f8af",
+    "url": "/react-homework-4/static/js/main.e4b1c07b.chunk.js"
+  },
+  {
+    "revision": "6fc25759d5556a00e457",
+    "url": "/react-homework-4/static/js/runtime-main.944a4856.js"
+  }
+]);
